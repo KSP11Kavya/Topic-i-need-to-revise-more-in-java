f16eb3a83483083ffb00cfda30d3a8f6859bc76d
kavya pandey <kavyapandey@kavyas-MacBook-Air.local>
2024-02-11 12:41:53 +0530

    final commit

---
+++ TypeConversion/Ques.java
@@ -0,0 +1,19 @@
+package TypeConversion;
+
+public class Ques {
+    public static void main(String[] args) {
+        //narrowing
+        double b = 99.4;
+        int c = (int)b;
+        System.out.println(b);
+        System.out.println(c);
+    //  //wide
+    // int a = 8;
+    // double p = a;
+    // System.out.println(a);   
+    // System.out.println(p);
+    }
+    
+
+    
+}
---
+++ TypeConversion/Ques1.java
@@ -0,0 +1,17 @@
+package TypeConversion;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        //narrow
+        double b = 65.6;
+        byte f = (byte)b;
+        System.out.println(b);
+        System.out.println(f);
+        // //wide
+        // byte n = 4;
+        // double m = n;
+        // System.out.println(n);
+        // System.out.println(m);
+    }
+    
+}
