b4a8d0221a3a51de6da73c096ef37ace180e53a4
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-11 13:51:32 +0530

    Create Ques.java

---
+++ AutoboxingandUnboxing/Ques.java
@@ -0,0 +1,15 @@
+package AutoboxingandUnboxing;
+
+public class Ques {
+    public static void main(String[] args) {
+    Integer a = new Integer(22);
+    int b = a;
+    }
+    // public static void main(String[] args) {
+    //     int a =10;
+    //     Integer b = a;
+    //     System.out.println(a);
+    //     System.out.println(b);
+    // }
+    
+}
