42c836043dd2aab276c0e513c61d367b2d9eb010
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-11 13:50:56 +0530

    Create Q2.java

---
+++ AutoboxingandUnboxing/Q2.java
@@ -0,0 +1,19 @@
+package AutoboxingandUnboxing;
+
+public class Q2 {
+    public static void main(String[] args) {
+        //autoboxing
+        int a = 99;
+        Integer b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    //unboxing
+    // public static void main(String[] args) {
+    //     Integer s = new Integer(33);
+    //     int k = s;
+    //     System.out.println(s);
+    //     System.out.println(k);
+    // }
+    
+}
