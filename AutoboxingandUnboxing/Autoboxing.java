c8aaa1a4ea71f34b67993d477fa294ad98406dce
kavya pandey <kavyapandey@kavyas-MacBook-Air.local>
2024-02-11 11:04:19 +0530

    second commit

---
+++ AutoboxingandUnboxing/Autoboxing.java
@@ -0,0 +1,10 @@
+package AutoboxingandUnboxing;
+public class Autoboxing {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer b= new Integer(a);
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ AutoboxingandUnboxing/Unboxing.java
@@ -0,0 +1,11 @@
+package AutoboxingandUnboxing;
+
+public class Unboxing {
+    public static void main(String[] args) {
+        Integer b = new Integer(56);
+        int a = b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
