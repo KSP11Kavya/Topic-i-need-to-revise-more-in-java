813b1600202027760d5e2efec9f5d0e444fbe334
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-24 03:15:57 +0530

    Update Test.java

--- Operators/Assignment_Operators/Test.java
+++ Operators/Assignment_Operators/Test.java
@@ -1,1 +1,20 @@
+package Operators.Assignment_Operators;
+
+public class Test {
+    //+=
+    //-=
+    // *=
+    // '/='
+    // %=
+    public static void main(String[] args) {
+        int a = 80;
+        int b =56;
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+        System.out.println(a%=b);
+
+    }
+}
 
