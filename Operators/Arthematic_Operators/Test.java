c92a52424d11847732aca53561948924a26f4684
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-24 03:04:53 +0530

    Update Test.java

--- Operators/Arthematic_Operators/Test.java
+++ Operators/Arthematic_Operators/Test.java
@@ -1,1 +1,17 @@
+package Operators.Arithmetic_Operators;
+
+public class Test {
+    public static void main(String[] args) {
+int a = 56;
+int b= 22;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a /b);
+System.out.println(a % b);
+System.out.println(a * b);
+    
+    }
+   
+    
+}
 
