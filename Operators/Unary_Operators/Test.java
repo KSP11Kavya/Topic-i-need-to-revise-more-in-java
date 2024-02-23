9cab2a9c8e9363a74963eb58f7edfe90e4899b35
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-24 03:16:51 +0530

    Update Test.java

--- Operators/Unary_Operators/Test.java
+++ Operators/Unary_Operators/Test.java
@@ -1,1 +1,25 @@
+package Operators.Unary_Operator;
+
+public class Test {
+    public static void main(String[] args) {
+        int a= 9;
+        --a;
+        System.out.println(a);
+        // int a= 9;
+        // a--;
+        // System.out.println(a);
+    //   int a = 10;
+    //   ++a;
+    //   System.out.println(a);
+        // int b =5;
+        // b++;
+        // System.out.println(b);
+//         // Example: Changing a happy block to a sad block
+// boolean happy = true; // Happy block!
+// boolean sohappy = !happy; // Now it's a sad block :(
+
+//         System.out.println(sohappy);
+    }
+    
+}
 
