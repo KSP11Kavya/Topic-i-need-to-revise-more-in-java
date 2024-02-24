076939542c04f1577e8c1e2caeaab6b22d02b1ae
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-24 12:09:13 +0530

    Update Test.java

--- Operators/Logical_Operators/Test.java
+++ Operators/Logical_Operators/Test.java
@@ -1,1 +1,21 @@
+package Operators.Logical_Operators;
+
+public class Test {
+   //&&
+   // ||
+   // !
+public static void main(String[] args) {
+    boolean snackFinished = true;
+    boolean roomCleaned = true;
+    // boolean playAllowed = snackFinished && roomCleaned;
+    // System.out.println(playAllowed);
+    boolean playAllowed = snackFinished || roomCleaned;
+    System.out.println(playAllowed);
+
+    boolean  tired = false;
+    boolean keepPlaying = !tired;
+    System.out.println(keepPlaying);
+}
+
+}
 
