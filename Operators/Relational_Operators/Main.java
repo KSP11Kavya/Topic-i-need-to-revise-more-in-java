ebe117d1aecbdfec74f41d3078d8bc659c0cb7fd
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-25 03:21:11 +0530

    Update Main.java

--- Operators/Relational_Operators/Main.java
+++ Operators/Relational_Operators/Main.java
@@ -1,1 +1,16 @@
+package Operators.Relational_Operators;
+
+public class Main {
+public static void main(String[] args) {
+    int a = 66;
+    int b = 99;
+    boolean c = a!=b;
+    //boolean c = a == b;
+    // boolean c = a <= b;
+    //boolean c = a >= b;
+    //boolean c = a < b;
+    // boolean c = a >b;
+    System.out.println(c);
+}    
+}
 
