e8af68e4d9e3e600353f54f3ec967f25e44319d7
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-24 12:48:12 +0530

    Update Test.java

--- Operators/Relational_Operators/Test.java
+++ Operators/Relational_Operators/Test.java
@@ -1,1 +1,47 @@
+package Operators.Relational_Operators;
+
+public class Test {
+    //==
+    //!=
+    //<
+    //<=
+    //>
+    //>=
+
+    public static void main(String[] args) {
+    // ==
+        int tower1Height = 10;
+    int tower2Height = 10;
+    boolean sameHeight = tower1Height == tower2Height;
+    System.out.println(sameHeight);
+    // !=
+    int myBridges =2;
+    int friendBridges = 3;
+    boolean differentBridges = myBridges != friendBridges;
+    System.out.println(differentBridges);
+    // >
+    int myDragonSize = 5;
+    int friendDragonSize = 3;
+    boolean biggerDragon = myDragonSize > friendDragonSize;
+    System.out.println(biggerDragon);
+    // <
+    int gatehouseSize = 2;
+    int mainHallSize = 4;
+    boolean smallerGatehouse = gatehouseSize < mainHallSize;
+    System.out.println(smallerGatehouse);
+    //>=
+        int towerHeight =10;
+        int wallHeight=10;
+        boolean tallTowers = towerHeight >= wallHeight;
+            System.out.println(tallTowers);
+        //<=
+
+    int windowBricks = 4;
+    int maxBricks = 5;
+    boolean smallWindows = windowBricks <= maxBricks;
+
+        
+    }
+    
+}
 
