54c058ac9fbc06be5c766547826d1f342ec2a0a6
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-25 03:26:42 +0530

    Update Main.java

--- Operators/Unary_Operators/Main.java
+++ Operators/Unary_Operators/Main.java
@@ -1,1 +1,15 @@
+package Operators.Unary_Operator;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        // --a;
+       // ++a;
+        //a--;
+        a++;
+        System.out.println(a);
+
+    }
+    
+}
 
