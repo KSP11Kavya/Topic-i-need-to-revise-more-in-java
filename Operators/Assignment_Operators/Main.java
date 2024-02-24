6dc35baca8bb9bfbf4f0d766bba34cd584d6e4fd
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-25 03:18:24 +0530

    Update Main.java

--- Operators/Assignment_Operators/Main.java
+++ Operators/Assignment_Operators/Main.java
@@ -1,1 +1,16 @@
+package Operators.Assignment_Operators;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 85;
+        int b = 72;
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a%=b);
+        System.out.println(a/=b);
+
+    }
+    
+}
 
