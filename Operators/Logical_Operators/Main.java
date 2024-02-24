ae0d1b91004711c5dd1565180fcfcadd02b42cfb
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-25 03:19:55 +0530

    Update Main.java

--- Operators/Logical_Operators/Main.java
+++ Operators/Logical_Operators/Main.java
@@ -1,1 +1,15 @@
+package Operators.Logical_Operators;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean ram = true;
+        boolean laxman = true ;
+        // boolean brothers = ram && laxman;
+        boolean brother = !ram;
+        boolean brothers = ram || laxman;
+        System.out.println(brothers);
+        System.out.println(brother);
+    }
+    
+}
 
