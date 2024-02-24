e835ab5b69b70b4fcb040601bcf38c84a86449f6
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-25 03:23:05 +0530

    Update Main.java

--- Operators/Ternary_Operators/Main.java
+++ Operators/Ternary_Operators/Main.java
@@ -1,1 +1,12 @@
-
+public class Main{
+    public static void main (String[] args){
+        int a = 55;
+        int b = 45;
+        int c = 52;
+        int result;
+        result
+        = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+    System.out.println("Max of three numbers = "
+                       + result);
+    }
+}
