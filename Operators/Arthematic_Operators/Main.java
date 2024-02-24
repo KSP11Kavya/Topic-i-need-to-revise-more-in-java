25c031b0a42643cc465655c15d86aa7a2dcbcaa7
KSP11Kavya <106591634+KSP11Kavya@users.noreply.github.com>
2024-02-25 03:16:13 +0530

    Update Main.java

--- Operators/Arthematic_Operators/Main.java
+++ Operators/Arthematic_Operators/Main.java
@@ -1,1 +1,16 @@
+package Operators.Arithmetic_Operators;
+
+public class Main {
+   public static void main(String[] args) {
+    int a = 80;
+    int b =99;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a%b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+   }
+    
+    
+}
 
